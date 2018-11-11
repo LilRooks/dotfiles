afb744afbac3652463e38d0127fc44bcdfbde73d
Anna Rooks <a.Git@ironflurry.com>
2018-11-11 11:33:42 -0500

    Reordered segments, root_indicator more easily seen

--- home/.oh-my-zsh/custom/power9k.zsh
+++ home/.oh-my-zsh/custom/power9k.zsh
@@ -1,5 +1,5 @@
-POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(vi_mode ssh dir vcs)
-POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator background_jobs history battery time)
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(vi_mode ssh root_indicator dir vcs)
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status background_jobs history battery time)
 
 POWERLEVEL9K_BATTERY_STAGES="▁▂▃▄▅▆▇█"
 POWERLEVEL9K_DIR_SHOW_WRITABLE=true
