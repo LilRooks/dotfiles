b754d6f9ca12a1bc24ad371634b5c838c615fbd5
Anna Rooks <a.Git@ironflurry.com>
2019-01-07 11:54:25 +0800

    fixed lock script being moved to i3 config folder

--- home/.config/i3/lock.sh
+++ home/.config/i3/lock.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 TMPBG=/tmp/screen.png
-LOCK=$HOME/Build/i3lock-blur/lock-smaller.png
+LOCK=$HOME/.config/i3/lock-smaller.png
 RES=$(xrandr | grep 'current' | sed -E 's/.*current\s([0-9]+)\sx\s([0-9]+).*/\1x\2/')
 
 ffmpeg -f x11grab -video_size $RES -y -i $DISPLAY -i $LOCK -filter_complex "scale=iw/15:ih/15,scale=15*iw:15*ih:flags=neighbor,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $TMPBG -loglevel quiet
