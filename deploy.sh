e817daea7c73ea6b18e69ff99da29124082c261f
Anna Rooks <git.ann@rooks.dev>
2020-05-21 16:08:52 -0400

    switching from urxvt to termite

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+[[ "$0" != "./deploy.sh" ]] && echo "Please run from dotfiles directory!" && exit 1
+
 checkFor() {
     echo "Checking to see if $1 is installed"
     if ! [ -x "$(command -v $1)" ]; then
@@ -8,29 +10,10 @@ checkFor() {
     fi
 }
 
-checkShell() {
-    if [ -z "${SHELL##*zsh*}" ] ;then
-            echo "Default shell is zsh."
-    else
-        echo "Default shell is not zsh. Do chsh -s \$(which zsh)."
-        exit 1
-    fi
-}
-
-cpConf() {
-    mv -v ~/.$1 ~/.$1.old
-    ln -sv "$PWD/.$1" ~/.$1
-}
-
-lnOMZmod() {
-    ln -sv "$PWD/zsh/$1" ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/$1
-}
-
 echo "Does the following:"
 echo "1. Check to make sure there's zsh, vim, and tmux installed"
-echo "2. Check to see if default shell is zsh"
-echo "3. Install zsh mods"
-echo "4. Synchronizes other dotfiles"
+echo "2. Install zsh mods"
+echo "3. Synchronizes other dotfiles"
 echo
 
 # 1. Check to make sure there's zsh, vim, and tmux installed
@@ -39,26 +22,21 @@ checkFor vim
 checkFor tmux
 echo
 
-# 2. Check to see if default shell is zsh
-checkShell
-echo
-
-# 3. Install zsh mods
+# 2. Install zsh mods
 curl -L git.io/antigen > $HOME/antigen.zsh
-#sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -qO -)"
-#rm ~/.zshrc
-#echo
 
-# 4. Synchronizes other dotfiles
+# 3. Synchronizes other dotfiles
 git clone https://github.com/gmarik/Vundle.vim ~/.vim/bundle/Vundle.vim
 
 mkdir ~/.config
 mkdir ~/.ssh
 
+mkdir -p ~/.local/share/fonts
+cp $PWD/home/.local/share/fonts/* ~/.local/share/fonts
+
 ln -sv $PWD/home/.* ~/
 ln -sv $PWD/home/.config/* ~/.config/
 ln -sv $PWD/home/.ssh/* ~/.ssh/
 ln -sv $PWD/home/.oh-my-zsh/* ~/.oh-my-zsh/
-#rsync -aPubz home/ ~/
 
 vim +PluginInstall +qall
